81ace61bce41ac830c08f2525acb8996ceda4534
Ashura100 <113298159+Ashura100@users.noreply.github.com>
2023-03-20 09:27:21 +0100

    Quinzième

--- Coin challenge/Assets/Scripts/Ennemis.cs
+++ Coin challenge/Assets/Scripts/Ennemis.cs
@@ -1,28 +1,102 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Ennemis : MonoBehaviour
 {
     [SerializeField]
     private AudioClip ZombieClip = null;
-    public Transform target;
-    public float speed = 4f;
+    [SerializeField] private Transform _movePosTransform;
+    private NavMeshAgent _navMeshAgent;
+    public Transform player;
+    public LayerMask playerMask, groundMask;
     Rigidbody rb;
     private AudioSource zombie_AudioSource;
-    // Start is called before the first frame update
-    void Start()
+    //patrouille
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+    //attaque
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+    //states
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+
+    public float health;
+    void Awake()
     {
+        player = GameObject.Find("Personnage").transform;
+        _navMeshAgent = GetComponent<NavMeshAgent>();
         rb = GetComponent<Rigidbody>();
         zombie_AudioSource = GetComponent<AudioSource>();
         zombie_AudioSource.PlayOneShot(ZombieClip);
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
-        Vector3 pos = Vector3.MoveTowards(transform.position, target.position, speed*Time.fixedDeltaTime);
-        rb.MovePosition(pos);
-        transform.LookAt(target);
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, playerMask);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, playerMask);
+
+        if (!playerInSightRange && !playerInAttackRange) Patrouille();
+        if (playerInSightRange && !playerInAttackRange) Poursuite();
+        if (playerInSightRange && playerInAttackRange) Attaque();
+
+    }
+    void Patrouille()
+    {
+        if (!walkPointSet) SearchWalkPoint();
+
+        if (walkPointSet)
+            _navMeshAgent.SetDestination(walkPoint);
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        if (distanceToWalkPoint.magnitude < 1f)
+            walkPointSet = false;
+    }
+
+    void SearchWalkPoint()
+    {
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, groundMask))
+            walkPointSet = true;
+    }
+    void Poursuite()
+    {
+        _navMeshAgent.SetDestination(player.position);
+    }
+
+    void Attaque()
+    {
+        _navMeshAgent.SetDestination(transform.position);
+
+        transform.LookAt(player);
+
+        if (!alreadyAttacked)
+        {
+            alreadyAttacked = true;
+            Invoke(nameof(ArretAttaque), timeBetweenAttacks);
+        }
+    }
+    void ArretAttaque()
+    {
+        alreadyAttacked = false;
+    }
+
+    void TakeDamage(int damage)
+    {
+        health -= damage;
+        if (health < 0) Invoke(nameof(Die), 5f);
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
     }
 }
